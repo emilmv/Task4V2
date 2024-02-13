79716fd6e12fbc4ddcd9fb35f83f5319c69b264e
emilmv <emilnm@code.edu.az>
2024-02-14 00:22:24 +0400

    Task4v2

---
+++ Task4v2.cs
@@ -0,0 +1,133 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#region Task1.1
+//int n = 10;
+//if (n % 2 == 0)
+//{
+//    Console.WriteLine("Cütdür");
+//}
+//else
+//{
+//    Console.WriteLine("Təkdir");
+//}
+#endregion
+#region Task1.2
+//int n = 10;
+//if (n % 21 == 0)
+//{
+//    Console.WriteLine("Həm 3, həm 7-ə bölünür");
+//}
+//else
+//{
+//    Console.WriteLine("Bölünmür");
+//}
+#endregion
+#region Task1.3
+//int x = 10;
+//int y = 12;
+//if (x % 2 == 0 && y % 0 == 0)
+//{
+//    Console.WriteLine(x + y);
+//}
+//else
+//{
+//    Console.WriteLine("Verilən ədədlərdən heç olmasa biri cüt deyil");
+//}
+#endregion
+#region Task1.4
+//double temp = 15;
+//if (temp >= 15)
+//{
+//    Console.WriteLine("Hava istidir");
+//}
+//else
+//{
+//    Console.WriteLine("Hava Soyuqdur");
+//}
+#endregion
+#region Task2.1
+//int x = 10;
+//int y = 12;
+//int z = 14;
+//int result = 0;
+//if (x > 10 && x < 100)
+//{
+//    result += x;
+//}
+//if(y>10&&y < 100)
+//{
+//    result += y;
+//}
+//if(z>10&&z < 100)
+//{
+//    result += z;
+//}
+//Console.WriteLine(result);
+#endregion
+#region Task2.2
+//int month = 10;
+//if (month > 0 &&month< 13)
+//{
+//    if (month >= 3 && month <= 5)
+//    {
+//        Console.WriteLine("Yaz");
+//    }
+//    if (month >= 6 && month <= 8)
+//    {
+//        Console.WriteLine("Yay");
+//    }
+//    if (month >= 9 && month <= 11)
+//    {
+//        Console.WriteLine("Payız");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Qış");
+//    }
+
+//}
+//else
+//{
+//    Console.WriteLine("Düzgün ədəd daxil edin");
+//}
+#endregion
+#region Task2.3
+//int num = 321;
+//if (num > 99 && num < 1000)
+//{
+//    int lastNum = num % 10;
+//    int secondNum = (num / 10) % 10;
+//    int thirdNum = num / 100;
+
+//    Console.WriteLine(lastNum);
+//    Console.WriteLine(secondNum);
+//    Console.WriteLine(thirdNum);
+//}
+//else
+//{
+//    Console.WriteLine("Düzgün ədəd daxil edin");
+//}
+#endregion
+#region Task2.4
+int x = 20;
+int y = 40;
+int z = 50;
+int result = 0;
+if (x > y)
+{
+    result += x;
+}
+else
+{
+    result += y;
+}
+if (y > z)
+{
+    result += y;
+}
+else
+{
+    result += z;
+}
+Console.WriteLine(result);
+#endregion
